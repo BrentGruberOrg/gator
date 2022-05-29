66589ec559ebb654ea15c617d4c853e76c458914
Brent Gruber <brent.gruber77@gmail.com>
2022-05-28 23:53:59 -0400

    format

--- main.tf
+++ main.tf
@@ -1,73 +1,73 @@
 resource "digitalocean_droplet" "caddy" {
-    count = var.count
-    image = var.image
-    name = "www-${count.index}"
-    region = var.region
-    size = var.size
-    ssh_keys = [
-      data.digitalocean_ssh_key.mbp.id,
-      data.digitalocean_ssh_key.desk.id,
-      data.digitalocean_ssh_key.mist.id
+  count  = var.count
+  image  = var.image
+  name   = "www-${count.index}"
+  region = var.region
+  size   = var.size
+  ssh_keys = [
+    data.digitalocean_ssh_key.mbp.id,
+    data.digitalocean_ssh_key.desk.id,
+    data.digitalocean_ssh_key.mist.id
+  ]
+
+  connection {
+    host        = self.ipv4_address
+    user        = "root"
+    type        = "ssh"
+    private_key = file(var.pvt_key)
+    timeout     = "2m"
+  }
+
+  provisioner "file" {
+    source      = "scripts/tailscale.sh"
+    destination = "/tmp/tailscale.sh"
+  }
+
+  provisioner "file" {
+    source      = "scripts/caddy.sh"
+    destination = "/tmp/caddy.sh"
+  }
+
+  provisioner "file" {
+    source      = "scripts/Caddyfile"
+    destination = "/tmp/Caddyfile"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/tailscale.sh",
+      "/tmp/tailscale.sh ${var.TAILSCALE_TOKEN}",
+      "chmod +x /tmp/caddy.sh",
+      "/tmp/caddy.sh"
     ]
-
-    connection {
-        host = self.ipv4_address
-        user = "root"
-        type = "ssh"
-        private_key = file(var.pvt_key)
-        timeout = "2m"
-    }
-
-    provisioner "file" {
-        source = "scripts/tailscale.sh"
-        destination = "/tmp/tailscale.sh"
-    }
-
-    provisioner "file" {
-        source = "scripts/caddy.sh"
-        destination = "/tmp/caddy.sh"
-    }
-
-    provisioner "file" {
-        source = "scripts/Caddyfile"
-        destination = "/tmp/Caddyfile"
-    }
-
-    provisioner "remote-exec" {
-        inline = [
-            "chmod +x /tmp/tailscale.sh",
-            "/tmp/tailscale.sh ${var.TAILSCALE_TOKEN}",
-            "chmod +x /tmp/caddy.sh",
-            "/tmp/caddy.sh"
-        ]
   }
 }
 
 resource "digitalocean_loadbalancer" "www-lb" {
-  name = "www-lb"
+  name   = "www-lb"
   region = var.region
 
   forwarding_rule {
-    entry_port = 80
+    entry_port     = 80
     entry_protocol = "http"
 
-    target_port = 80
+    target_port     = 80
     target_protocol = "http"
   }
 
   forwarding_rule {
-    entry_port = 443
+    entry_port     = 443
     entry_protocol = "https"
 
-    target_port = 443
+    target_port     = 443
     target_protocol = "https"
 
     tls_passthrough = true
   }
-  
+
 
   healthcheck {
-    port = 22
+    port     = 22
     protocol = "tcp"
   }
 
--- variables.tf
+++ variables.tf
@@ -3,14 +3,14 @@ variable "DO_DOMAIN" {}
 variable "TAILSCALE_TOKEN" {}
 variable "pvt_key" {}
 variable "image" {
-    default = "ubuntu-20-04-x64"
+  default = "ubuntu-20-04-x64"
 }
 variable "region" {
-    default = "nyc3"
+  default = "nyc3"
 }
 variable "size" {
-    default = "s-1vcpu-1gb"
+  default = "s-1vcpu-1gb"
 }
 variable "count" {
-    default = 1
+  default = 1
 }
